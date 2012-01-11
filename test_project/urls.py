85c15b11d35ea5e842b07095d2dbaa6c06573405
Mike Yumatov <mike@yumatov.org>
2012-01-11 11:58:10 +0400

    Fix test project urls

--- test_project/urls.py
+++ test_project/urls.py
@@ -4,13 +4,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 from lemon import extradmin
-from lemon.utils import urls
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(extradmin.site.urls)),
+    url(r'^tinymce/', include('tinymce.urls')),
     url(r'^robots\.txt', include('lemon.robots.urls')),
     url(r'^sitemap\.xml', include('lemon.sitemaps.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
