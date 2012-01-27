bd6a720ade96c86f075d3f8608ec98d279cb1a06
Mike Yumatov <mike@yumatov.org>
2012-01-27 17:43:12 +0400

    Fix chosen usage in admin

--- lemon/extradmin/assets/extradmin/js/select.js
+++ lemon/extradmin/assets/extradmin/js/select.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.field select')
+  $('.b-admin-form-field select')
     .width(function(index, width) { return $(this).attr('multiple') ? width + 150 : width + 50; })
     .chosen({disable_search_threshold: 50});
 });
