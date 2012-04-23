6a5a8076a4f0a9f1b9dd647fa6572cb894b46953
Mike Yumatov <mike@yumatov.org>
2012-04-23 11:10:37 +0400

    Fix admin widgets support for Django 1.4

--- lemon/extradmin/widgets.py
+++ lemon/extradmin/widgets.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.conf import settings
 from django.contrib.admin import widgets
+from django.contrib.admin.templatetags.admin_static import static
 from django.core.urlresolvers import reverse, NoReverseMatch
 from django.utils.translation import ugettext as _
 from django.utils.safestring import mark_safe
@@ -8,16 +9,16 @@ from django.utils.safestring import mark_safe
 
 class AdminDateWidget(widgets.AdminDateWidget):
 
-    class Media:
-        extend = ()
-        js = (settings.STATIC_URL + 'extradmin/js/jquery.datetimepicker.js',)
+    @property
+    def media(self):
+        return forms.Media(js=[static('extradmin/js/jquery.datetimepicker.js')])
 
 
 class AdminTimeWidget(widgets.AdminTimeWidget):
 
-    class Media:
-        extend = ()
-        js = (settings.STATIC_URL + 'extradmin/js/jquery.datetimepicker.js',)
+    @property
+    def media(self):
+        return forms.Media(js=[static('extradmin/js/jquery.datetimepicker.js')])
 
 
 class AdminSplitDateTime(widgets.AdminSplitDateTime):
@@ -30,27 +31,35 @@ class AdminSplitDateTime(widgets.AdminSplitDateTime):
 class ForeignKeyRawIdWidget(widgets.ForeignKeyRawIdWidget):
 
     def render(self, name, value, attrs=None):
+        rel_to = self.rel.to
         if attrs is None:
             attrs = {}
-        related_url = '../../../%s/%s/'
-        related_url %= (self.rel.to._meta.app_label,
-                        self.rel.to._meta.object_name.lower())
-        params = self.url_parameters()
-        if params:
-            url = '?' + '&amp;'.join(
-                ['%s=%s' % (k, v) for k, v in params.items()])
-        else:
-            url = ''
-        if not attrs.has_key('class'):
-            attrs['class'] = 'vForeignKeyRawIdAdminField'
-        output = [forms.TextInput.render(self, name, value, attrs)]
-        output.append(
-            '<a href="%s%s" class="related-lookup" id="lookup_id_%s" > ' % \
-                (related_url, url, name))
-        output.append(
-            '<img src="%simg/admin/selector-search.gif" width="16"' \
-            ' height="16" alt="%s" /></a>' % \
-                (settings.ADMIN_MEDIA_PREFIX, _('Lookup')))
+        extra = []
+        if rel_to in self.admin_site._registry:
+            related_url = reverse(
+                'admin:%s_%s_changelist' % (
+                    rel_to._meta.app_label,
+                    rel_to._meta.module_name
+                ),
+                current_app=self.admin_site.name,
+            )
+
+            params = self.url_parameters()
+            if params:
+                params = [u'%s=%s' % (k, v) for k, v in params.items()]
+                params = u'&amp;'.join(params)
+                url = u'?' + params
+            else:
+                url = u''
+            if 'class' not in attrs:
+                attrs['class'] = 'vForeignKeyRawIdAdminField'
+            extra.append(
+                u'<a href="%s%s" class="related-lookup" id="lookup_id_%s"> ' %
+                    (related_url, url, name))
+            extra.append(
+                u'<img src="%s" width="16" height="16" alt="%s" /></a>' %
+                    (static('admin/img/selector-search.gif'), _('Lookup')))
+        output = [forms.TextInput.render(self, name, value, attrs)] + extra
         if value:
             output.append(self.label_for_value(value))
         return mark_safe(u''.join(output))
@@ -61,21 +70,14 @@ class RelatedFieldWidgetWrapper(widgets.RelatedFieldWidgetWrapper):
     def render(self, name, value, *args, **kwargs):
         rel_to = self.rel.to
         info = (rel_to._meta.app_label, rel_to._meta.object_name.lower())
-        try:
-            related_url = reverse(
-                'admin:%s_%s_add' % info, current_app=self.admin_site.name)
-        except NoReverseMatch:
-            info = (self.admin_site.root_path, rel_to._meta.app_label,
-                    rel_to._meta.object_name.lower())
-            related_url = '%s%s/%s/add/' % info
         self.widget.choices = self.choices
         output = [self.widget.render(name, value, *args, **kwargs)]
-        if rel_to in self.admin_site._registry:
+        if self.can_add_related:
+            related_url = reverse('admin:%s_%s_add' % info, current_app=self.admin_site.name)
             output.append(
-                u'<a href="%s" class="add-another" id="add_id_%s"> ' % \
+                u'<a href="%s" class="add-another" id="add_id_%s"> ' %
                     (related_url, name))
             output.append(
-                u'<img src="%simg/admin/icon_addlink.gif" width="10" ' \
-                u' height="10" alt="%s"/></a>' % \
-                    (settings.ADMIN_MEDIA_PREFIX, _('Add Another')))
+                u'<img src="%s" width="10" height="10" alt="%s"/></a>' %
+                    (static('admin/img/icon_addlink.gif'), _('Add Another')))
         return mark_safe(u''.join(output))
