477b15a03749ce8058bff97dd2dfc8a809d7a623
Mike Yumatov <mike@yumatov.org>
2012-08-08 11:57:11 +0400

    Fix sitemap.xml data format

--- lemon/sitemaps/views.py
+++ lemon/sitemaps/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.utils import translation
 
 from lemon.sitemaps.models import Item
 
 
 def sitemap_xml(request):
+    translation.activate('en')
     qs = Item.objects.filter(sites=request.site, enabled=True)
     return render(request, 'sitemaps/sitemap.xml',
                   {'object_list': qs}, content_type='application/xml')
